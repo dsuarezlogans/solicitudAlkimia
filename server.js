b537daa20b9559cd834e2ccebe358465a7d8d332
Danny Suarez <dsuarezlogans@gmail.com>
2017-04-09 03:21:24 -0400

    configurando server para heroku

--- server.js
+++ server.js
@@ -5,19 +5,19 @@
     const express = require('express');
     const bodyParser = require('body-parser');
     const mongoose = require('mongoose');
-                     require('mongoose-schematypes-extend')(mongoose);                     
+                     require('mongoose-schematypes-extend')(mongoose);
     const cors = require('cors');
 
     //Inicializadores del Server
     const app = express();
-    const Port = process.env.PORT || 3030;
+    app.set('port', (process.env.PORT || 5000));
     const rutas = express.Router();
 
     // Conexion a la DB
     const MongoDB = process.env.PROD_MONGODB || 'mongodb://alkimia:Alkimia123.@ds155130.mlab.com:55130/solicitud';
     mongoose.connect(MongoDB, function(err, res) {
         if (err) throw err;
-        console.log('Conectado a MongoDB');
+        console.log('Conectado a mLab, mongoDB');
     });
 
     //Modelos de Datos
@@ -53,7 +53,7 @@
          .post(ctrlSolicitud.addSolicitud);
     rutas.route('/solicitud/:id')
          .get(ctrlSolicitud.findById)
-         .put(ctrlSolicitud.updateSolicitud)
+         .put(ctrlSolicitud.updateSolicitud);
     rutas.route('/solicitud/instancia/:instancia')
          .get(ctrlSolicitud.findByInstancia);
 
@@ -68,8 +68,7 @@
     app.use('/api', rutas);
 
     // Iniciar el server
-    var server = app.listen(Port, ()=> {
-        var port = server.address().port;
-        console.log("Servidor Node corriendo en http://localhost:", port);
+app.listen(app.get('port'), ()=> {
+        console.log("Servidor Node corriendo en Puerto", app.get('port'));
       });
 })();
