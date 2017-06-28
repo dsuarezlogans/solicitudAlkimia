2e114253a386e9dbc69d5eea311fdfcde7447deb
Danny Suarez <dsuarezlogans@gmail.com>
2017-06-27 20:42:59 -0400

    borrando logs de testing

--- controladores/ubicacion.js
+++ controladores/ubicacion.js
@@ -9,10 +9,8 @@
     const busqueda = req.body.direccion;
     console.log(busqueda);
     geocoder.geocode(busqueda, function(err, data) {
-      console.log(data);
       req.body.lat = data.results[0].geometry.location.lat;
-      req.body.lng = data.results[0].geometry.location.lng;
-      console.log(req.body);
+      req.body.lng = data.results[0].geometry.location.lng;      
       next();
     });
 
